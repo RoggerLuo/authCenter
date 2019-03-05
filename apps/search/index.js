6709daadf17b862166b39ecb55b90f134ebe1e53
Roger <luojie.5408@163.com>
2019-03-05 22:40:21 +0800

    update from mac pro

--- apps/search/index.js
+++ apps/search/index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const router = express.Router()
 const app = require('../note/app')
 router.get('/', app.search)
-router.get('/keywords',app.searchKeyword)
+router.post('/keywords',app.searchKeyword)
 module.exports = router
