83408caeefb66b2c24fa28d405b3afa872ad6c38
Roger <luojie.5408@163.com>
2019-03-14 11:15:47 +0800

    Update

--- inits/init_middleware.js
+++ inits/init_middleware.js
@@ -34,7 +34,7 @@ module.exports = function (app) {
   app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header('Access-Control-Allow-Credentials', true);
-    res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With,Content-Length,Authorization,Accept,Cookie,Cache-Control,Pragma,expire-day");
+    res.header("Access-Control-Allow-Headers", "token, Content-Type, X-Requested-With,Content-Length,Authorization,Accept,Cookie,Cache-Control,Pragma,expire-day");
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     const isLoginOrRegister = req.url.indexOf('/auth/verify')!==-1||req.url.indexOf('/auth/login')!==-1||req.url.indexOf('/auth/register')!==-1
     if(isLoginOrRegister) {
